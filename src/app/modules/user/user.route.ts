471839d43790f2e55817e3531f8f5014d9ae19b9
GitHub-Sanjit <sanjit.est14@gmail.com>
2024-07-09 13:31:07 +0600

    part2

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,8 +1,15 @@
-import express from "express";
-import { UserControllers } from "./user.controller";
+/* eslint-disable no-console */
+import express from 'express';
+import { UserControllers } from './user.controller';
+import { crateStudentValidationSchema } from '../student/student.validation';
+import validateRequest from '../../middlewares/validateRequest';
 
 const router = express.Router();
 
-router.post('/create-student', UserControllers.createStudent);
+router.post(
+  '/create-student',
+  validateRequest(crateStudentValidationSchema),
+  UserControllers.createStudent,
+);
 
 export const UserRoutes = router;
