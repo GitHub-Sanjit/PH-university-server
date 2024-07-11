89a9c0ed1a29f03ac9eba9b440bc174d97ecf962
GitHub-Sanjit <sanjit.est14@gmail.com>
2024-07-11 13:56:11 +0600

    Part 3 Complete

---
+++ src/app/modules/academicFaculty/academicFaculty.route.ts
@@ -0,0 +1,27 @@
+import express from 'express';
+import validateRequest from '../../middlewares/validateRequest';
+import { AcademicFacultyValidations } from './academicFaculty.Validation';
+import { AcademicFacultyControllers } from './academicFaculty.controller';
+const router = express.Router();
+
+router.post(
+  '/create-academic-faculty',
+  validateRequest(
+    AcademicFacultyValidations.createAcademicFacultyValidationSchema,
+  ),
+  AcademicFacultyControllers.createAcademicFaculty,
+);
+
+router.get('/:facultyId', AcademicFacultyControllers.getSingleAcademicFaculty);
+
+router.patch(
+  '/:facultyId',
+  validateRequest(
+    AcademicFacultyValidations.upateAcademicFacultyValidationSchema,
+  ),
+  AcademicFacultyControllers.updateAcademicFaculty,
+);
+
+router.get('/', AcademicFacultyControllers.getAllAcademicFaculties);
+
+export const AcademicFacultyRoutes = router;
--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -2,6 +2,8 @@ import { Router } from 'express';
 import { AcademicSemesterRoutes } from '../modules/academicSemester/academicSemester.route';
 import { StudentRoutes } from '../modules/student/student.route';
 import { UserRoutes } from '../modules/user/user.route';
+import { AcademicFacultyRoutes } from '../modules/academicFaculty/academicFaculty.route';
+import { AcademicDepartmentRoutes } from '../modules/academicDepartment/academicDepartment.route';
 
 const router = Router();
 
@@ -14,10 +16,18 @@ const moduleRoutes = [
     path: '/students',
     route: StudentRoutes,
   },
-    {
-      path: '/academic-semesters',
-      route: AcademicSemesterRoutes,
-    },
+  {
+    path: '/academic-semesters',
+    route: AcademicSemesterRoutes,
+  },
+  {
+    path: '/academic-faculties',
+    route: AcademicFacultyRoutes,
+  },
+  {
+    path: '/academic-departments',
+    route: AcademicDepartmentRoutes,
+  },
 ];
 
 moduleRoutes.forEach((route) => router.use(route.path, route.route));
